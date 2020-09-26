7c951b180c366933901b5c560e5e70248d9255cf
technopulse <46396446+technopulse@users.noreply.github.com>
2020-09-26 16:51:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# myteacher.tech
+# myteacher
+ 
---
+++ index1.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    
+    <!--====== Title ======-->
+    <title>Let's Help - Build A House</title>
+    
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/png">
+        
+    <!--====== Magnific Popup CSS ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        
+    <!--====== Slick CSS ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+        
+    <!--====== Line Icons CSS ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+        
+    <!--====== Bootstrap CSS ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== Default CSS ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style CSS ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+</head>
+
+<body>
+    <!--[if IE]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+   
+    <!--====== PRELOADER PART START ======-->
+
+    <div class="preloader">
+        <div class="loader">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--====== PRELOADER PART ENDS ======-->
+    
+    
+    
+    <!--====== PRINICNG START ======-->
+
+    <section id="pricing" class="">
+        <div class="container">
+
+            <div class="row justify-content-center">
+
+                <div class="col-lg-12 col-md-12 col-sm-9">
+                    <div class="pricing-style mt-30" style="background: linear-gradient(#8dde9f 0%, #0067f4 100%);">
+                        <div class="pricing-icon text-center">
+                            <img src="assets/images/abdur Rahman.jpeg" alt="">
+                        </div>
+                        <div class="pricing-list">
+<center><ul style="font-family: 'Comic Sans MS', cursive, sans-serif;font-size:14pt; ">
+	<li><i class="lni lni-check-mark-circle"></i> <b style="font-size: 15px;color: white;">Let's Help</b></li>
+	<li  style="font-size: 20px;"><i class="lni lni-check-mark-circle"></i> BUILD A HOUSE</li>
+<li><img src="assets/images/home.jpeg" alt=""></li>
+	<li style="font-size: 15px;color: white;"><i class="lni lni-check-mark-circle"></i> For Someone who helped us</li>
+	<li style="font-size: 15px;color: white;"><i class="lni lni-check-mark-circle"></i> Build a Career</li>
+	<li style="font-size: 15px;color: white;"><i class="lni lni-check-mark-circle"></i> Abdur Rahman Sir had just began construction of his house before his sudden Demise.<br>So Let us</li>
+	<li style="font-size: 20px;color: white;"><i class="lni lni-check-mark-circle"></i> FULFILL A DREAM</li>
+	<li style="font-size: 15px;color: white;"><i class="lni lni-check-mark-circle"></i> of Someone who helped us</li>
+	<li style="font-size: 20px;color: white;"><i class="lni lni-check-mark-circle"></i> Fulfill our Dream</li>
+</ul></center>
+							
+                        </div>
+<div class="pricing-btn rounded-buttons text-center row">
+<table style="width: 100%;">
+<tr>
+
+
+
+<td><a class="btn btn-success" href="upi://pay?pa=9353573541@ybl&pn=RehmanSir&tid=<?php echo rand(); ?>&tr=Donor2000&am=2000&tn=Help to Build a House&cu=INR&url=https://myteacher.tech/success.php&mode=05&orgid=000000">Donate <br> Rs. 2000</a><hr></td>
+<td><a class="btn btn-success" href="upi://pay?pa=9353573541@ybl&pn=RehmanSir&tid=<?php echo rand(); ?>&tr=Donor5000&am=5000&tn=Help to Build a House&cu=INR&url=https://myteacher.tech/success.php&mode=05&orgid=000000">Donate <br> Rs. 5000</a><hr></td>
+<tr>
+<td><a class="btn btn-success" href="upi://pay?pa=9353573541@ybl&pn=RehmanSir&tid=<?php echo rand(); ?>&tr=Donor10000&am=10000&tn=Help to Build a House&cu=INR&url=https://myteacher.tech/success.php&mode=05&orgid=000000">Donate <br> Rs. 10000</a></td>
+<td><a class="btn btn-success" href="upi://pay?pa=9353573541@ybl&pn=RehmanSir&tid=<?php echo rand(); ?>&tr=AnyAmount&tn=Help to Build a House&cu=INR&url=https://myteacher.tech/success.php&mode=05&orgid=000000">Any Other <br>Amount</a></td>
+</tr>
+</table>
+</div>
+                    </div> <!-- pricing style one -->
+                </div>
+                
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
+
+    <!--====== PRINICNG ENDS ======-->
+    
+    
+    
+    <!--====== BACK TOP TOP PART START ======-->
+
+    <a href="#" class="back-to-top"><i class="lni lni-chevron-up"></i></a>
+
+    <!--====== BACK TOP TOP PART ENDS ======-->    
+
+    <!--====== PART START ======-->
+
+<!--
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+-->
+
+    <!--====== PART ENDS ======-->
+
+
+
+
+    <!--====== Jquery js ======-->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.7.1.min.js"></script>
+    
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    
+    <!--====== Ajax Contact js ======-->
+    <script src="assets/js/ajax-contact.js"></script>
+    
+    <!--====== Isotope js ======-->
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    
+    <!--====== Scrolling Nav js ======-->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+    
+	
+	
+<script>
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+</script>
+
+</body>
+
+</html>
